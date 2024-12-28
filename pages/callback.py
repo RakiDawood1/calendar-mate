d23f60bf22f4eb9f010c0e4aa615cbbac5e79734
Rakhan Dawood <rakhandawood@Rakhans-MacBook-Pro.local>
2024-12-29 00:52:29 +0530

    Update auth flow and add callback

--- pages/callback.py
+++ pages/callback.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-if 'token' in st.session_state:
-    st.success("Successfully authenticated!")
-    st.script_html("<script>window.close();</script>")
+st.success("Authentication successful!")
+if st.query_params.get("code"):
+    st.session_state["oauth_code"] = st.query_params["code"]
+    st.script_html("""
+        <script>
+        window.close();
+        window.opener.location.reload();
+        </script>
+    """)
